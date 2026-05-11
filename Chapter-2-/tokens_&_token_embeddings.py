7804c6089d9492010c633af845d031c363307f9a
Boutmedjet Abd elmoudjib <smailhd2019@gmail.com>
2026-05-11 23:41:27 +0100

    feat(tokenizer) : Comparing Trained LLM Tokenizers
    Bert ( cased 2018) vs Bert( uncased 2018 ) vs GPT-2 (2019 ) vs Flan-T5 (2022) vs GPT-4 (2023) vs StarCoder2 (2024)
    vs Galactica vs Phi-3 (and Llama 2)

--- Chapter-2-/tokens_&_token_embeddings.py
+++ Chapter-2-/tokens_&_token_embeddings.py
@@ -31,3 +31,44 @@ print(tokenizer.decode(3323))
 print(tokenizer.decode(622))
 print(tokenizer.decode([3323, 622]))
 print(tokenizer.decode(29901))
+
+# Comparing Trained LLM Tokenizers
+
+colors_list = [
+    '102;194;165', '252;141;98', '141;160;203',
+    '231;138;195', '166;216;84', '255;217;47'
+]
+
+def show_tokens(sentence,tokenizer_name):
+  tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+  token_ids = tokenizer(sentence).input_ids
+  for idx, t in enumerate(token_ids):
+            print(
+            f'\x1b[0;30;48;2;{colors_list[idx % len(colors_list)]}m' +
+            tokenizer.decode(t) +
+            '\x1b[0m',
+            end=' '
+        )
+
+text = """
+English and CAPITALIZATION
+🎵 鸟
+show_tokens False None elif == >= else: two tabs:"    " Three tabs: "       "
+12.0*50=600
+"""
+
+show_tokens(text, "bert-base-uncased")
+
+show_tokens(text, "bert-base-cased")
+
+show_tokens(text, "gpt2")
+
+show_tokens(text, "google/flan-t5-small")
+
+show_tokens(text, "Xenova/gpt-4")
+
+show_tokens(text, "bigcode/starcoder2-15b")
+
+show_tokens(text, "facebook/galactica-1.3b")
+
+show_tokens(text, "microsoft/Phi-3-mini-4k-instruct")
